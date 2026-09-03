92c25a2d488ff24a7b08b7ffa88c334083b5ba49
Imanol Ortiz <imanolof29@gmail.com>
2026-09-03 17:22:38 +0200

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Imanol Ortiz on 03/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
